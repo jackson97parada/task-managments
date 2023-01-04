84b356c68761232725b675dba43af210dad30741
jackson Arevalo <jackson97parada@gmail.com>
2023-01-04 16:09:44 -0500

    first fail

--- db/migrate/20230104175342_create_employees.rb
+++ db/migrate/20230104175342_create_employees.rb
@@ -1,8 +1,8 @@
 class CreateEmployees < ActiveRecord::Migration[7.0]
   def change
     create_table :employees do |t|
-      t.string :name, limit: 45
-      t.string :last_name, limit: 45
+      t.string :name, null: false, limit: 45
+      t.string :last_name, null: false, limit: 45
       t.string :document null: false, unique: true
       t.string :mobile null: false, unique: true, limit: 10
       t.float :salary
